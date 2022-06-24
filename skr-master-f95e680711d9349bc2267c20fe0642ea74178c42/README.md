29a135580ca5482947da8e233e39999a8cd3339d
lqz <1798307825@qq.com>
2022-06-24 22:19:15 +0800

    提交轮播与修改的顶部

---
+++ skr-master-f95e680711d9349bc2267c20fe0642ea74178c42/README.md
@@ -0,0 +1,61 @@
+# app
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+|__node_modules
+|__public
+  |__favicon.ico
+  |__index.html
+|__src
+  |__api(接口)
+  |__assets(样式)
+     |__less
+  |__components (放置全局组件) 
+  |__filters(放置过滤器)
+     |__index.js
+  |__icons(图标)
+     |__svg(字体图标存放位置)
+  |__router(路由配置)
+     |__index.js
+  |__store(vuex配置)
+     |__index.js
+  |__utils(存放工具类函数)
+     |__rquest.js(封装网络请求)
+     |__localstlroage.js(实现永久化存储)
+  |__views(存放路由组件实例对象)
+  |__pages(页面组件)
+  |__app.js
+  |__main.js
+  |__permission.js(权限管理)
+|__.browserslistrc
+|__.eslintrc.js
+|__babel.config.js
+|__.gitignore
+|__favicon.ico
+|__jsconfig.json
+|__package-lock.json
+|__package.json
+|__README.md
+|__vue.config.js(配置代理)
---
+++ skr-master-f95e680711d9349bc2267c20fe0642ea74178c42/src/components/Lunbo.vue
@@ -0,0 +1,64 @@
+<template>
+<div class="carousel">
+   <el-carousel  height="938px" width="938px" indicator-position="outside" arrow="always" 
+  
+   >
+     <el-carousel-item v-for="(src,index) in Images" :key="index">
+      <img :src="src" alt="">
+    </el-carousel-item>
+  </el-carousel>
+</div>
+</template>
+
+<script>
+import a from '../assets/images/a.jpg'
+import b from '../assets/images/b.jpg'
+import c from '../assets/images/c.jpg'
+import d from '../assets/images/d.jpg'
+export default {
+   name:'lunbo',
+   data() {
+    return {
+        Images:[
+          a,b,c,d
+        ]
+    }
+   },
+}
+</script>
+
+<style>
+.el-carousel__arrow{
+z-index: 10000;
+width: 56px;
+height: 56px;
+}
+.carousel{
+  float: left;
+  position: relative;
+  left: -260px;
+  height: 938px;
+  width: 938px;
+  cursor: pointer;
+}
+.el-carousel-item {
+    color: #475669;
+    font-size: 18px;
+    opacity: 0.75;
+    line-height: 938px;
+    margin: 0;
+  }
+  img{
+    width: 938px;
+    height: 938px;
+  }
+  
+  .el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+  }
+  
+  
+  .el-carousel__item:nth-child(2n+1) {
+    background-color: #d3dce6;
+  }
+</style>
