ef3c19b5a434383be769dc80bf328e3c6a401e39
支吾 <619756832@qq.com>
2022-06-24 09:39:54 +0800

    更新(添加新的全局组件)

---
+++ src/components/ShopItem/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="shop_item">
+    <div class="shop_img">
+      <img :src="shop.img" alt="" />
+    </div>
+    <div class="shop_item_details">
+      <div class="shop_title">{{shop.title}}</div>
+      <div class="shop_price">{{shop.price}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ShopItem",
+  props:{
+    shop:Object,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.shop_item {
+  margin: 4px;
+  width: 176px;
+  height: 280px;
+  border: 1px solid #ccc;
+  background-color: #fff;
+  cursor: pointer;
+  &:hover{
+    transform: scale(1.007);
+    box-shadow: 0 0 4px 4px #eee;
+  }
+  img {
+    width: 176px;
+    height: 176px;
+  }
+  .shop_item_details {
+    padding: 24px;
+    text-align: center;
+    .shop_title {
+      color: #333;
+      margin-bottom: 8px;
+      line-height: 1.5;
+      list-style: none;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+      overflow: hidden;
+    }
+    .shop_price{
+        color: #666;
+        font-family: 'yg740';
+        font-size: 12px;
+        line-height: 1.5;
+        word-break: keep-all;
+    }
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -5,7 +5,9 @@ import store from './store'
 import './assets/scss/master.scss'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import ShopItem from './components/ShopItem/index.js'
 
+Vue.use(ShopItem)
 Vue.config.productionTip = false;
 Vue.use(ElementUI);
 
